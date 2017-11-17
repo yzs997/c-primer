aa7ad05410c6091a2272a0132988e3b7e064d30f
yzs997 <33413639+yzs997@users.noreply.github.com>
2017-11-17 18:42:03 +0800

    Update ex_13_08.h

--- chp13/ex_13_08.h
+++ chp13/ex_13_08.h
@@ -5,8 +5,10 @@
 class HasPtr {
 public:
 	HasPtr(const std::string &s = std::string()) : ps(new std::string(s)), i(0) {}
-	HasPtr(const HasPtr &rhs) : ps(new std::string(*rhs.ps)), i(rhs.i) {} //exercise 13.5
-	HasPtr &operator=(const HasPtr &rhs) // exercise 13.8
+	//exercise 13.5
+	HasPtr(const HasPtr &rhs) : ps(new std::string(*rhs.ps)), i(rhs.i) {}
+	// exercise 13.8
+	HasPtr &operator=(const HasPtr &rhs)
 	{									 
 		if (this != &rhs)
 		{
