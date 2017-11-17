47e001ab5c5a2f90242832cfb4e219df88355e82
yzs997 <33413639+yzs997@users.noreply.github.com>
2017-11-17 19:41:54 +0800

    Create ex_13_17_for_ex_13_15.cpp

---
+++ chp13/ex_13_17/ex_13_17_for_ex_13_15.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+class numbered
+{
+public:
+	numbered() { mysn = st_id++; }
+	int mysn;
+	numbered(const numbered &rhs) { mysn = st_id++; }
+private:
+	static int st_id;
+};
+
+int numbered::st_id = 0;
+
+void f(numbered s)
+{
+	cout << s.mysn << endl;
+}
+int main()
+{
+	numbered a, b = a, c = b;
+	f(a);
+	f(b);
+	f(c);
+}
