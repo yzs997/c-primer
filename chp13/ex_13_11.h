b16c6251ba79a7e80c09cb9661f05b7c14189c96
yzs997 <33413639+yzs997@users.noreply.github.com>
2017-11-17 18:41:27 +0800

    Create ex_13_11.h

---
+++ chp13/ex_13_11.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <string>
+
+class HasPtr {
+public:
+	HasPtr(const std::string &s = std::string()) : ps(new std::string(s)), i(0) {}
+	//exercise 13.5
+	HasPtr(const HasPtr &rhs) : ps(new std::string(*rhs.ps)), i(rhs.i) {}
+	// exercise 13.8
+	HasPtr &operator=(const HasPtr &rhs)
+	{
+		if (this != &rhs)
+		{
+			delete ps;
+			ps = new std::string(*rhs.ps);
+			i = rhs.i;
+		}
+		return *this;
+	}
+	//exercise 13.11
+	~HasPtr()							
+	{
+		delete ps;
+	}
+private:
+	std::string *ps;
+	int i;
+};
