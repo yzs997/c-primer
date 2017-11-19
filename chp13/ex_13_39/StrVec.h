d791f3c379313cbb00aaf8c658f87244fbe011c5
yzs997 <33413639+yzs997@users.noreply.github.com>
2017-11-19 21:13:17 +0800

    Create StrVec.h

---
+++ chp13/ex_13_39/StrVec.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <string>
+#include <memory>
+
+class StrVec
+{
+public:
+	StrVec() : elements(nullptr), first_free(nullptr), cap(nullptr) {}
+	StrVec(const StrVec&);
+	StrVec &operator=(const StrVec&);
+	~StrVec();
+	void push_back(const std::string&);
+	size_t size() const { return first_free - elements; }
+	size_t capacity() const { return cap - elements; }
+	std::string *begin() const { return elements; }
+	std::string *end() const { return first_free; }
+	void reserve(std::size_t);
+	void resize(std::size_t);
+	void resize(size_t, const std::string&);
+private:
+	std::allocator<std::string> alloc;
+	std::string *elements;
+	std::string *first_free;
+	std::string *cap;
+private:
+	void chk_n_alloc();
+	std::pair<std::string*, std::string*> alloc_n_copy(const std::string*, const std::string*);
+	void free();
+	void reallocate();
+};
