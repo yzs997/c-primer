bc2018694272f21e7376887a2c9dae3d97e71111
yzs997 <33413639+yzs997@users.noreply.github.com>
2017-11-19 18:05:54 +0800

    Create ex_13.30.h

---
+++ chp13/ex_13.30.h
@@ -0,0 +1,71 @@
+#pragma once
+
+
+
+#include <string>
+#include <iostream>
+
+class HasPtr;
+
+void swap(HasPtr &lhs, HasPtr &rhs);
+
+
+class HasPtr {
+	friend void swap(HasPtr &lhs, HasPtr &rhs);
+
+public:
+
+	HasPtr(const std::string &s = std::string()) : ps(new std::string(s)), i(0) {}
+
+	//exercise 13.5
+
+	HasPtr(const HasPtr &rhs) : ps(new std::string(*rhs.ps)), i(rhs.i) {}
+
+	// exercise 13.8
+
+	HasPtr &operator=(const HasPtr &rhs)
+
+	{
+
+		if (this != &rhs)
+
+		{
+
+			delete ps;
+
+			ps = new std::string(*rhs.ps);
+
+			i = rhs.i;
+
+		}
+
+		return *this;
+
+	}
+
+	//exercise 13.11
+
+	~HasPtr()
+
+	{
+
+		delete ps;
+
+	}
+
+private:
+
+	std::string *ps;
+
+	int i;
+
+};
+
+inline
+void swap(HasPtr &lhs, HasPtr &rhs)
+{
+	using std::swap;
+	swap(lhs.ps, rhs.ps);
+	swap(lhs.i, rhs.i);
+	std::cout << "cal void swap(HasPtr &lhs, HasPtr &rhs)" << std::endl;
+}
